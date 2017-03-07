75b2fa6e7fbd9f218959d326572f7d4e93d5de81
floatfeather <lilianwangran@163.com>
2017-03-07 05:00:29 -0800

    put the deletion of result file forward

--- valgrind/fpdebug/script/fpdebug.sh
+++ valgrind/fpdebug/script/fpdebug.sh
@@ -2,10 +2,10 @@
 
 # This script assumes that you use and only use the VALGRIND_PRINT_VALUES once in your file.
 
-valgrind --tool=fpdebug --goto-shadow-branch=yes $@ > shadow.fd.temp 2>&1
-valgrind --tool=fpdebug $@ > original.fd.temp 2>&1
-
 rm -rf fpdebug_relerr.log
+
+valgrind --tool=fpdebug --goto-shadow-branch=yes --track-int=yes $@ > shadow.fd.temp 2>&1
+valgrind --tool=fpdebug $@ > original.fd.temp 2>&1
 
 # g++ fd_relerr.cpp -lmpfr -lgmp -o fd_relerr
 /home/lillian/work/install_fpdebug/valgrind-3.7.0/fpdebug/script/fd_relerr
